3228e7dca0baa71426670213bbf86cef19020a96
Clinton Davelaar <davelaarprogramming@gmail.com>
2021-02-06 16:45:30 -0600

    Initial Commit

---
+++ ClintonResumeSwiftUI/ClintonResumeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClintonResumeSwiftUIApp.swift
+//  ClintonResumeSwiftUI
+//
+//  Created by student on 2/6/21.
+//
+
+import SwiftUI
+
+@main
+struct ClintonResumeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
