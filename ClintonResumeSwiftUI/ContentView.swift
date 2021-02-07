1ae91dba2846c555631c61dce05d1911425ba4ce
Clinton Davelaar <davelaarprogramming@gmail.com>
2021-02-06 19:07:29 -0600

    Integrated tabs

--- ClintonResumeSwiftUI/ContentView.swift
+++ ClintonResumeSwiftUI/ContentView.swift
@@ -55,14 +55,98 @@ class NetworkManager: ObservableObject {
 }
 
 struct ContentView: View {
+    
     @ObservedObject var nManager = NetworkManager()
+    
+    @State var selectedIndex = 2
+    @State var showModal = false
+    
+    let tabBarImageNames = ["house.fill", "gear", "graduationcap.fill", "pencil", "line.horizontal.3"]
+    let titles = ["Home", "Experience", "Education", "Skills", "About Me"]
+    
+    init() {
+        UITabBar.appearance().barTintColor = .white
+    }
+    
     var body: some View {
-        NavigationView {
-            List{
-                ForEach(nManager.resume, id: \.self){res in
-                    Text(res.loc)
+        VStack(spacing: 0) {
+            ZStack {
+                
+                Spacer()
+                    .fullScreenCover(isPresented: $showModal, content: {
+                        Button(action: {
+                            showModal.toggle()
+                        }, label: {
+                            /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+                        })
+                    })
+                ForEach(nManager.resume, id: \.self){ res in
+                    switch selectedIndex {
+                    case 0:
+                        HomeView()
+                    case 2:
+                        EducationView(education: res.education)
+                        
+                    default:
+                        NavigationView {
+                            Text("On tab: \(selectedIndex)")
+                                .navigationTitle("Tab \(selectedIndex)")
+                        }
+                    }
                 }
             }
+            
+            Spacer()
+            
+            Divider()
+                .padding(.bottom, 16)
+            
+            HStack {
+                ForEach(0..<5) { num in
+                    Button(action: {
+                        selectedIndex = num
+                    }, label: {
+                        Spacer()
+                        VStack {
+                            Image(systemName: tabBarImageNames[num])
+                                .font(.system(size: 24, weight:. bold))
+                                .foregroundColor(selectedIndex == num ? .black : .gray)
+                                .padding(.bottom, 2)
+                            (selectedIndex == num) ? Text(titles[num]).font(.system(size: 10)).foregroundColor(.black) : Text("")
+                        }
+                        Spacer()
+                    })
+                }
+            }
+        }
+    }
+}
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            Text("On tab: 1")
+                .navigationTitle("Home")
+        }
+    }
+}
+
+struct EducationView: View {
+    
+    let education:[Education]
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(education, id: \.self){edu in
+                    VStack (alignment:.leading){
+                        Text(edu.at)
+                            .font(.headline)
+                        Text(edu.city)
+                    }
+                }
+            }
+                .navigationTitle("Education")
         }
     }
 }
